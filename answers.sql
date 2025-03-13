bb477fa726b9df865e6c8aa43b82576be4d04e8b
Kelvin Kimotho <89358855+Kelvinkimotho@users.noreply.github.com>
2025-03-13 06:58:52 -0700

    Update answers.sql
    Updated my answers

--- answers.sql
+++ answers.sql
@@ -4,8 +4,9 @@
 
 -- 2. Write an SQL query to retrieve the **orderDate**, **requiredDate**, and **status** of orders that are currently 'In Process' from the **orders** table. Sort the results in **descending** order of **orderDate**
     select orderDate, requiredDate, status from orders where status ='In Process'  order by orderDate DESC;
+
 -- 3. Write a query to display the **firstName**, **lastName**, and **email** of employees whose job title is 'Sales Rep' and order them in **descending** order of **employeeNumber**.  
-    select firstName, lastName, email from employees where job_title='Sales Rep' order by employeeNumber desc;
+    select firstName, lastName, email from employees where "job title"='Sales Rep' order by employeeNumber desc;
 
 -- 4. Write a query to retrieve **all** the columns and records from the **offices** table.  
      select * from offices;
